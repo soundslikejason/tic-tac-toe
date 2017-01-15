90f7f5d5314ee645109bfb0572d3df134da2c5fd
Jason Leong <soundslikejason@users.noreply.github.com>
2017-01-15 14:36:07 -0800

    win, lose, or tie

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 
   $("input:radio[name=player]").click(function() {
     player = $(this).val();
+    reset();
   });
 
   $("#reset").click(function() {
@@ -33,9 +34,19 @@ $(document).ready(function() {
   function playerTurn(player) {
 		counter ++;
 		if (player === "X") {
+      win = winner("X");
+      if (win) {
+        alert("YOU WIN!");
+        reset();
+      }
 			player = "O";
 		  computerTurn(player);
 		} else {
+      win = winner("O");
+      if (win) {
+        alert("YOU WIN!");
+        reset();
+      }
 			player = "X";
 			computerTurn(player);
 		}
@@ -46,13 +57,57 @@ $(document).ready(function() {
 		var random = board[Math.floor(Math.random() * board.length)];
 		if (player === "X") {
 			$("#cell" + random).text("X");
+      win = winner("X");
 		} else {
 			$("#cell" + random).text("O");
+      win = winner("O");
 		}
+    if (win) {
+      alert("YOU LOSE!");
+      reset();
+    }
     var indexValue = parseInt(random, 10);
     var index = board.indexOf(indexValue);
     board.splice(index, 1);
   }
+
+  function winner(player){
+  	if ($("#cell1").text() === player && $("#cell2").text() === player &&
+  		$("#cell3").text() === player) {
+  		return true;
+  	}
+	  if ($("#cell4").text() === player && $("#cell5").text() === player &&
+			$("#cell6").text() === player) {
+			return true;
+	  }
+	  if ($("#cell7").text() === player && $("#cell8").text() === player &&
+			$("#cell9").text() === player) {
+			return true;
+		}
+		if ($("#cell1").text() === player && $("#cell4").text() === player &&
+			$("#cell7").text() === player) {
+			return true;
+		}
+		if ($("#cell2").text() === player && $("#cell5").text() === player &&
+			$("#cell8").text() === player) {
+			return true;
+		}
+		if ($("#cell3").text() === player && $("#cell6").text() === player &&
+			$("#cell9").text() === player) {
+			return true;
+		}
+		if ($("#cell1").text() === player && $("#cell5").text() === player &&
+			$("#cell9").text() === player) {
+			return true;
+		}
+		if ($("#cell3").text() === player && $("#cell5").text() === player &&
+			$("#cell7").text() === player) {
+			return true;
+		}
+		if (counter === 9 && winner !== true) {
+			alert("TIE!");
+		}
+	}
 
   function reset() {
     for (var i = 1; i <= 9; i++) {
